7794928507fb5904c4f40398e739b3b3bffbf91e
Chong <aaa59891@gmail.com>
2018-05-29 11:28:08 +0800

    Updated design recipe html Added recipe model

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../../../models/recipe.model';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: "app-recipe-list",
+    templateUrl: "./recipe-list.component.html",
+    styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('Test', 'Test description', 'http://1.bp.blogspot.com/_cP3Pd1BRVXc/R1g9Icp9R9I/AAAAAAAACME/KItpcxCgRHg/s400/peppermint+cookies+R.jpg')
+    ];
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
